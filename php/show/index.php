9a3ee6d7403bc32c6661a7e26bb5b30b98dad96b
Michael Koch <mikommd@gmail.com>
2021-11-23 17:59:16 +0100

    Add files via upload

--- php/show/index.php
+++ php/show/index.php
@@ -1,12 +1,14 @@
 <?php
+header("Content-type: text/html; charset=utf-8");
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
 include "config.php";
 
 $query=mysqli_query($connect, "SELECT * FROM files");
 while($data = mysqli_fetch_array($query)){
-    echo $data["filename"]."<br>".$data["desc1"]."<br>____________________<br/>";
+    echo $data["filename"]."<br/>".$data["desc1"]."<br/>".$data["filetype"]."<br/>____________________<br/>";
 }
 
 
